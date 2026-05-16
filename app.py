71e1054dd7b069c5bac4d512709cbca795bac9e4
OmegaHR System <omega@localhost>
2026-05-16 22:03:50 +0200

    Force trigger pro Render

--- app.py
+++ app.py
@@ -182,3 +182,4 @@ def seasons(): return render_ui("<h2 class='text-center text-xl font-black'>TURN
 def hof(): return render_ui("<h2 class='text-center text-xl font-black text-yellow-500'>SÍŇ SLÁVY</h2>")
 
 if __name__ == '__main__': app.run(debug=True, host='0.0.0.0', port=5000)
+# Force deploy trigger
