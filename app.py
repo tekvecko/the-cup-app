c42604e46bde0fa9433141b0c906a71c3609445f
OmegaHR System <omega@localhost>
2026-05-17 22:47:42 +0200

    Kompletní obnova stránky detailu turnaje s veškerou logikou

--- app.py
+++ app.py
@@ -865,6 +865,9 @@ if __name__ == '__main__': app.run(debug=True, host='0.0.0.0', port=5000)
 
 
 
+
+
+
 DETAIL_UI = MATCH_MACRO + """<div id="live-sync-container" data-tid="{{ tournament.id }}">
 <div id="export-area" class="w-full pb-4">
 
